1fc4699a91dd62f608f01fd5048c326f6a92434f
Theodor Tonum <theodor@tonum.no>
2014-11-14 22:42:55 +0100

    Lint repository using hound
    Enter the meta

--- lib/hound-rubocop.rb
+++ lib/hound-rubocop.rb
@@ -1,4 +1,4 @@
-require 'rubocop'
+require "rubocop"
 
 module HoundRubocop
   DEFAULT_CONFIG_FILE = File.expand_path("../../config/rubocop.yml", __FILE__)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-require 'simplecov'
+require "simplecov"
 
 module SimpleCov::Configuration
   def clean_filters
@@ -8,22 +8,14 @@ end
 
 SimpleCov.configure do
   clean_filters
-  load_adapter 'test_frameworks'
+  load_profile "test_frameworks"
 end
 
 ENV["COVERAGE"] && SimpleCov.start do
   add_filter "/.rvm/"
 end
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-require 'rspec'
-require 'hound-rubocop'
-
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-
-RSpec.configure do |config|
-
-end
+require "rspec"
+require "hound-rubocop"
