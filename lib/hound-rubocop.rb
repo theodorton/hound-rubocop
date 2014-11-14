a3dda1a64a9dc6545b842a1af735d414b6666ba8
Theodor Tonum <theodor@tonum.no>
2014-11-15 00:11:13 +0100

    Add hound:config rails generator

--- lib/hound-rubocop.rb
+++ lib/hound-rubocop.rb
@@ -3,3 +3,5 @@ require "rubocop"
 module HoundRubocop
   DEFAULT_CONFIG_FILE = File.expand_path("../../config/rubocop.yml", __FILE__)
 end
+
+require 'hound_rubocop/railtie' if defined?(Rails)
---
+++ lib/hound_rubocop/generators/config_generator.rb
@@ -0,0 +1,18 @@
+module Hound
+  class ConfigGenerator < ::Rails::Generators::Base
+    desc "Generates configuration matching Hound for Rubocop"
+
+    source_root File.expand_path('../templates', __FILE__)
+
+    def copy_rubocop_config
+      hound_defaults = HoundRubocop::DEFAULT_CONFIG_FILE
+
+      copy_file hound_defaults, ".rubocop.hound.yml"
+      copy_file ".rubocop.yml"
+    end
+
+    def copy_hound_config
+      copy_file ".hound.yml"
+    end
+  end
+end
